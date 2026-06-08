41d7814437a127d747b34c3e94ed57b3a3a1425c
codenano077 <shettydarshan021@gmail.com>
2026-06-08 20:06:23 +0530

    updated

--- obj/Debug/net10.0/MVCMovie.AssemblyInfo.cs
+++ obj/Debug/net10.0/MVCMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVCMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b0487a633a1ad7bc38f1e299ff4ac78a37cffac")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVCMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVCMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
